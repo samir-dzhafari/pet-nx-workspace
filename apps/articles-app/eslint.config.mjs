e491a5c6625c54c8eb13234ab3cfa4eab610aac9
Samir <samirjafari2001@icloud.com>
2025-09-30 19:07:01 +0300

    lint off

--- apps/articles-app/eslint.config.mjs
+++ apps/articles-app/eslint.config.mjs
@@ -1,7 +1,3 @@
-import baseConfig from '@ssheverev/eslint-config';
-import reactConfig from '@ssheverev/eslint-react-config';
-import tsConfig from '@ssheverev/eslint-ts-config';
-import nx from '@nx/eslint-plugin';
+import nx from "@nx/eslint-plugin";
 
-export default [...baseConfig, ...tsConfig, ...reactConfig, ...nx.configs['flat/react']];
-
+export default [...nx.configs["flat/react"]];
--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -1,15 +1,8 @@
-import baseConfig from '@ssheverev/eslint-config';
-import reactConfig from '@ssheverev/eslint-react-config';
-import tsConfig from '@ssheverev/eslint-ts-config';
-import nx from '@nx/eslint-plugin';
+import nx from "@nx/eslint-plugin";
 
 export default [
-  ...baseConfig,
-  ...tsConfig,
-  ...reactConfig,
-  ...nx.configs['flat/react'],
+  ...nx.configs["flat/react"],
   {
-    ignores: ['**/out-tsc'],
+    ignores: ["**/out-tsc"],
   },
 ];
-
